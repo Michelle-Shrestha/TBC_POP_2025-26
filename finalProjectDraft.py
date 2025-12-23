09f0ba00d40ee6c487148516f96b6f20f10ffe80
Michelle Shrestha <michelleshrestha17@gmail.com>
2025-12-23 18:11:49 +0545

    Draft

---
+++ finalProjectDraft.py
@@ -0,0 +1,12 @@
+class StockItem:
+
+    def getStockName():
+    def stockDescription():
+    def increaseStock():
+    def sellStock():
+    def calc_VAT():
+    def setPrice():
+    def getPrice():
+    def __str__():
+
+class NavSys:
