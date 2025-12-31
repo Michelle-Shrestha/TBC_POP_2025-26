0599b5f8e416d2a4007d4885000043268a8d8767
Michelle Shrestha <michelleshrestha17@gmail.com>
2025-12-31 12:52:26 +0545

    draft update

--- finalProjectDraft.py
+++ finalProjectDraft.py
@@ -1,18 +1,18 @@
 class StockItem:
     stockCategory=  "Car Accessories"
-    def __init__(self,stockCode,itemQuantity,itemPrice):
+    def __init__(self,stockCode,itemQuantity=0,itemPrice=0):
         self.__stockCode = stockCode
         self.__itemQuantity= itemQuantity
         self.__itemPrice = itemPrice
 
         #Remainder for no quantity 
-        if itemQuantity>1:
+        if itemQuantity<0:
             print(f"No Quantity: {self.__itemQuantity}")
             raise ValueError("Item out of stock!!!")
 
     #Setters
     def setStockCode(self,stockCode):
-        self.__stockCode==stockCode
+        self.__stockCode=stockCode
     def setItemQuantity(self,itemQuantity):
         self.__itemQuantity=itemQuantity
     def setItemPrice(self,itemPrice):
@@ -29,17 +29,55 @@ class StockItem:
     #Methods 
 
     #Calculating price with vat
-    def calc_VAT(self,vat=17.5):
+    def calculate_VAT(self,vat=17.5):
         new_price = self.__itemPrice * (1+ vat/100)
-    def increaseStock(self):
-        pass
-    def sellStock(self):
-        pass
+        return new_price
+    
+    def increaseStock(self,qty):
+        ItemCapacity= 100-self.__itemQuantity
+        if qty<=0:
+            raise ValueError ("Cannot add quantity less than 1!!!")
+        if qty>ItemCapacity:
+            raise ValueError(f"Excess Quantity!!! You can add additional {ItemCapacity} only.")
+        #Checks whether the total Item quantity is valid or not (1-100)
+        if qty>0 and qty<=ItemCapacity:
+            self.__itemQuantity += qty
+            return f"{qty}Quantity Successfully Added!!! \nCurrent Quantity: {self.__itemQuantity}."
+
+    def sellStock(self,qty):
+        if qty<=0:
+            raise ValueError ("Invalid Quantity!!!\nQuantity must be greater than 0!!!")
+        if qty>self.__itemQuantity:
+            raise ValueError (f"Low Stock!!!\nAvailable Stock: {self.__itemQuantity}")
+        # If stock to be sell is valid more than 1 and less or equal to the available stock
+        if qty>0 and qty<=self.__itemQuantity:
+            self.__itemQuantity-=qty
+            return(f"{qty} Quantity Successfully Sold!!!\nCurrent Quantity: {self.__itemQuantity}")
+
     def getStockName(self):
         return "Unknown Stock Name"
-    def stockDescription(self):
-        return "Uknown Stock Description"
+    def getstockDescription(self):
+        return "Unknown Stock Description"
     def __str__(self):
         pass
 
 
+def displayMenu():
+    print("--- Car Parts and Accessories Shop---")
+    print("1. Adding New  Items                 ")
+    print("2. Modifying Items                   ")
+    print("5. Increase Stock                    ")
+    print("5. Sell Stock                        ")
+    print("6. Stock Details                     ")# __str__
+
+try:
+    s1 = StockItem("W101",10,1000)
+    print(s1.stockCategory)
+    print(s1.getStockName())
+    print(s1.getstockDescription())
+    print(s1.sellStock(0))
+    print(s1.increaseStock(0))
+    print(s1.calculate_VAT())
+    
+except ValueError as e:
+    print(e)
